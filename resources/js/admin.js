1dc4c3c16bca237364af26aff5264ef318db96e9
ErfanMohammadpour <thisiserfun@gmail.com>
2026-02-20 02:09:31 +0330

    maked login page responsive

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -3,9 +3,10 @@
 import "./admin/bootstrap.min.js";
 import "./admin/jquery-3.7.1.min.js";
 import "./admin/bootstrap-notify.js";
-import Chart from "chart.js/auto";
-import "./admin/Chart.extension.js";
+import { Chart, registerables } from "chart.js";
+Chart.register(...registerables);
 window.Chart = Chart;
+import "./admin/Chart.extension.js";
 import "./admin/choices.min.js";
 import "./admin/fullcalendar.min.js";
 import "./admin/perfect-scrollbar.min.js";
